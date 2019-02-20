6eb7abc6b9907192ac0207de1aec8d84334f4e1e
evgi <evgi88@gmail.com>
2019-02-20 14:18:45 -0700

    Add the testStuff function

--- app/src/main/java/com/example/gradplanproject/MainActivity.java
+++ app/src/main/java/com/example/gradplanproject/MainActivity.java
@@ -10,4 +10,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void testStuff() {
+        System.out.println("Android Studio marks 'Evgeniy' as a spelling mistake :D");
+    }
 }
