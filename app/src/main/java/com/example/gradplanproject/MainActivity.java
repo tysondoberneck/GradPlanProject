246a9b1c4223a44e9043382abc485e900bd375dd
Gyralithe <wom17002@byui.edu>
2019-02-21 18:24:25 -0700

    a better reply

--- app/src/main/java/com/example/gradplanproject/MainActivity.java
+++ app/src/main/java/com/example/gradplanproject/MainActivity.java
@@ -15,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("Android Studio marks 'Evgeniy' as a spelling mistake :D");
         System.out.println("It marks Bekker the same way...");
         System.out.println("Doberneck is a real word guys");
+        System.out.println("Womble's the only real name.");
     }
 }
