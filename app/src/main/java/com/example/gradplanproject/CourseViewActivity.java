fed8deaa4f88fc8d41fea8942887d4f330fb0a9a
Gyralithe <wom17002@byui.edu>
2019-02-26 16:27:26 -0700

    New Activity shells (I hope...)

---
+++ app/src/main/java/com/example/gradplanproject/CourseViewActivity.java
@@ -0,0 +1,13 @@
+package com.example.gradplanproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CourseViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_view);
+    }
+}
--- app/src/main/java/com/example/gradplanproject/LoadingActivity.java
+++ app/src/main/java/com/example/gradplanproject/LoadingActivity.java
@@ -3,12 +3,12 @@ package com.example.gradplanproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoadingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_loading);
     }
 
     public void testStuff() {
