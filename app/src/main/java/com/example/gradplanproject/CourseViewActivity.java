a074a0c2695f612ceeae737a2f95d34f6daeffa6
Your Name <acfinlayson98@gmail.com>
2019-02-26 18:35:43 -0700

    test

--- app/src/main/java/com/example/gradplanproject/CourseViewActivity.java
+++ app/src/main/java/com/example/gradplanproject/CourseViewActivity.java
@@ -10,4 +10,5 @@ public class CourseViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_view);
     }
+    // comment
 }
