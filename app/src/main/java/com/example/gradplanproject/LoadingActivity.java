e8dc27441fd776c61cdd30abcb7212c89db68e05
Gyralithe <wom17002@byui.edu>
2019-02-26 17:48:42 -0700

    In process of adding MockSleeper class to simulate loading times for testing

--- app/src/main/java/com/example/gradplanproject/LoadingActivity.java
+++ app/src/main/java/com/example/gradplanproject/LoadingActivity.java
@@ -1,7 +1,9 @@
 package com.example.gradplanproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class LoadingActivity extends AppCompatActivity {
 
@@ -11,10 +13,25 @@ public class LoadingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_loading);
     }
 
+    /*@Override
+    protected void onResume() {
+        super.onResume();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Log.d("Loading", "Loading finished; starting next activity...");
+
+        Intent intent = new Intent(this, CourseViewActivity.class);
+        startActivity(intent);
+    }*/
+
     public void testStuff() {
         System.out.println("Android Studio marks 'Evgeniy' as a spelling mistake :D");
         System.out.println("It marks Bekker the same way...");
         System.out.println("Doberneck is a real word guys");
     }
 }
-// Test Comment
