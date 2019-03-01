6bf407dde0a102b6521ebc63cffda4c190a48c6f
Your Name <acfinlayson98@gmail.com>
2019-02-28 18:45:22 -0700

    Two JUNIT test class stubs

---
+++ app/src/test/java/com/example/gradplanproject/TestFiltering.java
@@ -0,0 +1,12 @@
+package com.example.gradplanproject;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestFiltering {
+    @Test
+    public void testFilter() {
+        //Test searching activity
+    }
+}
---
+++ app/src/test/java/com/example/gradplanproject/TestJSON.java
@@ -0,0 +1,12 @@
+package com.example.gradplanproject;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestJSON {
+    @Test
+    public void testClass() {
+        // test loading from LoadCoarseList class
+    }
+}
