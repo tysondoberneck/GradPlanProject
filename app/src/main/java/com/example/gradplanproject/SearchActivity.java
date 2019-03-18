750de6b7e34c780ea291fdf0aebfcec8a87421c9
Gyralithe <wom17002@byui.edu>
2019-03-18 09:08:34 -0600

    Cleaned up SearchActivity (leftover code from previous tests)

--- app/src/main/java/com/example/gradplanproject/SearchActivity.java
+++ app/src/main/java/com/example/gradplanproject/SearchActivity.java
@@ -42,7 +42,6 @@ public class SearchActivity extends AppCompatActivity {
         catch(Exception e) {
             Log.e(TAG, e.getMessage());
         }
-        //
 
         // Filler text for recyclerView. Eventually, the recyclerView should instead take in the
         // intended list of Courses, which will be, basically, dressed-up Maps
@@ -54,27 +53,18 @@ public class SearchActivity extends AppCompatActivity {
         map1.put("name", "Software Design and Development");
         map1.put("details", "Burton - MWF - 9:00 AM");
         courseListExample.add(map1);
-//        courseListExample.get(0).put("code", "CS235");
-//        courseListExample.get(0).put("name", "Software Design and Development");
-//        courseListExample.get(0).put("details", "Burton - MWF - 9:00 AM");
 
         Map<String, String> map2 = new HashMap<>();
         map2.put("code", "FDREL225");
         map2.put("name", "Foundations of the Restoration");
         map2.put("details", "Taylor - TR - 7:45 AM");
         courseListExample.add(map2);
-//        courseListExample.get(1).put("code", "FDREL225");
-//        courseListExample.get(1).put("name", "Foundations of the Restoration");
-//        courseListExample.get(1).put("details", "Taylor - TR - 7:45 AM");
 
         Map<String, String> map3 = new HashMap<>();
         map3.put("code", "MATH341");
         map3.put("name", "Linear Algebra");
         map3.put("details", "Nelson - MWF - 11:30 AM");
         courseListExample.add(map3);
-//        courseListExample.get(2).put("code", "MATH341");
-//        courseListExample.get(2).put("name", "Linear Algebra");
-//        courseListExample.get(2).put("details", "Nelson - MWF - 11:30 AM");
 
         courseListExample.add(map1);
         courseListExample.add(map2);
@@ -91,7 +81,6 @@ public class SearchActivity extends AppCompatActivity {
         adapter = new RecyclerAdapter(courseListExample);
         recyclerView.setAdapter(adapter);
 
-
         // Filling each Spinner with appropriate text (from strings.xml) using ArrayAdapter
 
         Spinner spinner1 = findViewById(R.id.spinner1);
@@ -105,18 +94,6 @@ public class SearchActivity extends AppCompatActivity {
                 R.array.afterTime_list, android.R.layout.simple_spinner_item);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter(adapter2);
-
-//        Spinner spinner3 = findViewById(R.id.spinner3);
-//        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
-//                R.array.day_list, android.R.layout.simple_spinner_item);
-//        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner3.setAdapter(adapter3);
-//
-//        Spinner spinner4 = findViewById(R.id.spinner4);
-//        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this,
-//                R.array.instructor_list, android.R.layout.simple_spinner_item);
-//        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner4.setAdapter(adapter4);
     }
 
     public void testSpinnerData(View view) {
