52647b3ab9708d99da7ac1a479ec71ba2cf5aac1
Gyralithe <wom17002@byui.edu>
2019-03-21 12:10:46 -0600

    Added "add" button to each RecyclerView item

--- app/src/main/java/com/example/gradplanproject/SearchActivity.java
+++ app/src/main/java/com/example/gradplanproject/SearchActivity.java
@@ -69,19 +69,19 @@ public class SearchActivity extends AppCompatActivity {
         Map<String, String> map1 = new HashMap<>();
         map1.put("code", "CS235");
         map1.put("name", "Software Design and Development");
-        map1.put("details", "Burton - MWF - 9:00 AM");
+        map1.put("details", "Section 01: Burton - MWF - 9:00-10:00 AM");
         courseListExample.add(map1);
 
         Map<String, String> map2 = new HashMap<>();
         map2.put("code", "FDREL225");
         map2.put("name", "Foundations of the Restoration");
-        map2.put("details", "Taylor - TR - 7:45 AM");
+        map2.put("details", "Section 03: Taylor - TR - 7:45-8:45 AM");
         courseListExample.add(map2);
 
         Map<String, String> map3 = new HashMap<>();
         map3.put("code", "MATH341");
         map3.put("name", "Linear Algebra");
-        map3.put("details", "Nelson - MWF - 11:30 AM");
+        map3.put("details", "Section 07: Nelson - MWF - 11:30-12:30 PM");
         courseListExample.add(map3);
 
         courseListExample.add(map1);
